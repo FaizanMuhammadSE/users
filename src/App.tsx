05ef55a7c6e014315587fec1af1b94fcc8743a63
FaizanMuhammadSE <jutt41697@gmail.com>
2024-08-06 18:01:52 +0500

    Custom Font, MUI Theme added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Typography, ThemeProvider } from '@mui/material';
+import { theme } from './theme';
+
 function App() {
-  return <>Coming Soon</>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Typography variant='h1'>Coming Soon</Typography>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/theme/index.ts
@@ -0,0 +1,61 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#000',
+    },
+  },
+  typography: {
+    fontFamily: 'Inter, Roboto, sans-serif',
+    fontSize: 17.5, // 12.5px
+    h1: {
+      fontSize: '2.4rem',
+      fontWeight: 700,
+    },
+    h3: {
+      fontSize: '1.8rem',
+      fontWeight: 700,
+    },
+    h4: {
+      fontSize: '1.8rem',
+      fontWeight: 500,
+    },
+    h5: {
+      fontSize: '1.4rem',
+      fontWeight: 500,
+    },
+    h6: {
+      fontSize: '1.25rem',
+      fontWeight: 500,
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 20,
+          textTransform: 'none',
+          fontSize: 14,
+        },
+      },
+    },
+    MuiChip: {
+      styleOverrides: {
+        root: {
+          borderRadius: 5,
+        },
+      },
+    },
+  },
+  spacing: 8,
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
