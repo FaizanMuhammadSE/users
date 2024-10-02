e0727b55543874015b8698376a12d2e671db6ff6
FaizanMuhammadSE <jutt41697@gmail.com>
2024-10-02 11:57:58 +0500

    manifest file creation on build - this file provides key:value pair for original file name and after build what's its hashed name

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import sass from 'sass';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    manifest: true,
+  },
   css: {
     preprocessorOptions: {
       scss: {
